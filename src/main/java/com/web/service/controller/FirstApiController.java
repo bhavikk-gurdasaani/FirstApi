8d04bbaf5db9ecffc086fef03cddaff3c6196f75
bhavikk-gurdasaani <bhavikjob@gmail.com>
2019-02-12 19:39:32 +0530

    first commit

---
+++ src/main/java/com/web/service/controller/FirstApiController.java
@@ -0,0 +1,15 @@
+package com.web.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstApiController {
+	
+	@RequestMapping(value = "/getData", method = RequestMethod.GET)
+	public String getData() {
+		
+		return "Happy Learning";
+	}
+}
